161fe78af45a8dae4a9413a1f3714762b8d89461
rajesh-kayal-dev <rk6289943975@gmail.com>
2026-03-31 11:40:33 +0530

    Add create task API

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,34 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+interface Task {
+    id:number,
+    title:string,
+    status: "pending" | "completed";
+}
+let tasks: Task[] = [];
+
+
+app.post("/tasks", (req, res) => {
+  const { title } = req.body;
+
+  const newTask: Task = {
+    id: Date.now(),
+    title,
+    status: "pending"
+  };
+
+  tasks.push(newTask);
+
+  res.json({
+    message: "Task created",
+    task: newTask
+  });
+});
+
+
 app.get("/", (req, res)=>{
     res.send("server is running")
 })
