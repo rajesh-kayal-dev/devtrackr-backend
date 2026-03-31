704c1bcc6122004b5388e54c6eb59841c789f451
rajesh-kayal-dev <rk6289943975@gmail.com>
2026-03-31 13:02:31 +0530

    Add login API with JWT authentication

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,17 +1,16 @@
 import express from "express";
-
 import {
     createTask,
     getTask,
     updateTask,
     deleteTask
 } from "../controllers/task.controller";
-
+import { authMiddleware } from "../middleware/auth.middleware";
 const router = express.Router();
 
-router.post("/", createTask);
-router.get("/", getTask);
-router.patch("/:id", updateTask);
-router.delete("/:id", deleteTask);
+router.post("/", authMiddleware, createTask);
+router.get("/", authMiddleware, getTask);
+router.patch("/:id", authMiddleware, updateTask);
+router.delete("/:id", authMiddleware, deleteTask);
 
 export default router;
